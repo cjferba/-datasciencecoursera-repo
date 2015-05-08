db1dc2e911df75fba5e17099984e0843f1e979d6
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-05-08 19:57:21 +0200

    week 1
    download files and load data

--- Getting and Cleaning Data/Start.R
+++ Getting and Cleaning Data/Start.R
@@ -1,1 +1,18 @@
 ## Carlos Jesús Fernandez Basso
+
+if(! file.exists('data')){
+  dir.create('data')
+}
+download.file("data/")
+
+fileUrl<- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl, destfile = "./data/cameras.csv", method="internal")
+list.files("./data")
+
+dataDownloaded<- date()
+
+
+cameraData <-  read.table("./data/cameras.csv" , sep=",",header = T)
+
+
+head(cameraData)
