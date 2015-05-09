6d9d376f044a75eb116b357a51db8d9dd48e38c4
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-05-09 13:34:22 +0200

    Excel file

--- Getting and Cleaning Data/Week 1.R
+++ Getting and Cleaning Data/Week 1.R
@@ -1,27 +1,53 @@
 ## Carlos Jesús Fernandez Basso
 
+#librarys
+if(!require("xlsx")){
+  install.packages ("xlsx")
+  library("xlsx")
+}
+
 if(! file.exists('data')){
   dir.create('data')
 }
 
 ############################################################################
-## Download data
+## CSV format
 ############################################################################
+
+##Download data
 fileUrl<- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
 download.file(fileUrl, destfile = "./data/cameras.csv", method="internal")
 list.files("./data")
 
 dataDownloaded<- date()
 
-############################################################################
 ## Load with read.table
-############################################################################
 cameraData <-  read.table("./data/cameras.csv" , sep=",",header = T)
 head(cameraData)
 
-############################################################################
+
 ## Load with read.csv
-############################################################################
 cameraData <-  read.csv("./data/cameras.csv" )
 head(cameraData)
 
+
+
+############################################################################
+## Excel format
+############################################################################
+
+##Download data
+fileUrl<- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD"
+download.file(fileUrl, destfile = "./data/cameras.xlsx", method="internal")
+list.files("./data")
+
+dataDownloaded<- date()
+
+## Load with read.xlsx
+if (!file.exists(".data/cameraData.xlsx")) {
+  cameraData.xlsx <- read.xlsx("./data/cameras.xlsx", sheetIndex=1, header = TRUE)
+}
+
+cameraData.xlsx <-  read.xlsx("./data/cameras.xlsx" ,rowIndex = 1:7 ,#colIndex = 2:3,
+                         sheetIndex = 1,header = T)
+head(cameraData.xlsx)
