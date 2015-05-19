b44bf28ed7e3d56fa555164619e68603575fed36
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-05-19 17:11:55 +0200

    Load data

---
+++ Exploratory Data Analysis/Proyect 2/Load_Data.R
@@ -0,0 +1,14 @@
+#Load data if no exist
+if (!"neiData" %in% ls()) {
+  neiData <- readRDS("data/summarySCC_PM25.rds")
+}
+if (!"sccData" %in% ls()) {
+  sccData <- readRDS("./data/Source_Classification_Code.rds")
+}
+#show neiData
+head(neiData)
+#show sccData
+head(sccData)
+
+print(paste("Dimension of NEI Data: ",dim(neiData)[1],dim(neiData)[2] ))
+print(paste("Dimension of SCC Data: ",dim(sccData)[1],dim(sccData)[2] ))
